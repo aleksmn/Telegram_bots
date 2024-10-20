f4e440e5b6dc0500e6891bb1d307e3d59fce87b2
aleksmn <al-mn@yandex.ru>
2024-10-20 10:46:03 +0300

    add appointment

--- procedure_bot/main.py
+++ procedure_bot/main.py
@@ -1,4 +1,6 @@
 import telebot
+import json
+
 from config import api_token
 
 
@@ -10,6 +12,24 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=["start"])
 def handle_start(message):
     bot.send_message(message.chat.id, "Бот для записи на процедуры запущен.")
+
+
+def add_appointment(date, time, client):
+    # Чтение существующих данных из файла
+    try:
+        with open('data.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        data = {"appointments": [], "review": []}
+
+    # Добавление новой записи
+    new_appointment = {'date': date, 'time': time, 'client': client}
+    data['appointments'].append(new_appointment)
+
+    # Запись обновленных данных в файл
+    with open('data.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False)
+
 
 
 if __name__ == "__main__":
