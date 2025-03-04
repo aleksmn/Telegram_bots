609ada4f00fe2e80a4bde7d538fa8a491981a4bd
aleksmn <al-mn@yandex.ru>
2025-03-04 20:37:30 +0300

    set reminder on time

--- remainder/main_schedule.py
+++ remainder/main_schedule.py
@@ -21,16 +21,17 @@ def schedule_thread():
 @bot.message_handler(commands=["remind"])   
 def set_reminder(message):
     # Пример команды
-    # /remind 3600 напоминание
+    # /remind 20:32
     chat_id = message.chat.id
-    parts = message.text.split(' ', 2)
-    delay = int(parts[1])  # Время в секундах
-    reminder_message = parts[2]  # Сообщение напоминания
+    parts = message.text.split()
+    reminder_time = parts[1]
+
 
     def regular_remainder():
-        bot.send_message(chat_id, reminder_message)
+        bot.send_message(chat_id, "Напоминание")
 
-    schedule.every(1).seconds.do(regular_remainder)
+    # schedule.every(1).seconds.do(regular_remainder)
+    schedule.every().day.at(reminder_time).do(regular_remainder)
 
 
 
